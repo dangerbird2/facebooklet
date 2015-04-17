475d64bf239a5dac46c4e00cd04318497746cea1
Steve Shea <stevenlsjr@gmail.com>
2015-04-16 19:57:38 -0500

    added doc files

---
+++ include/facebooklet/face.h
@@ -0,0 +1,11 @@
+#ifndef FACE_H
+#define FACE_H
+
+#include "FaceBooklet.h"
+#include "interfaces.h"
+#include "FBProfile.h"
+#include "Database.h"
+#include "NodeData.h"
+#include "utils.h"
+
+#endif // FACE_H
