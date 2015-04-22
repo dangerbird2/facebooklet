8b25759a32222d878cfbf2659644e0554913730c
Steve Shea <stevenlsjr@gmail.com>
2015-04-22 10:09:20 -0500

    added main function and github page setup

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+//
+// Created by Steven on 4/22/15.
+//
+#include "face.h"
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+  int n;
+  std::cout << "give me a number\n";
+  std::cin >> n;
+  std::cout << n <<"\n";
+  return 0;
+}
